b70f57a93f691075d9d6a2a24ff733945cbbc71e
Shailendra <shailendra@propertyguru.com.sg>
2017-07-11 02:34:24 +0800

    mongodb duplicate insertion fixed

--- app.js
+++ app.js
@@ -11,13 +11,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
-
+//app.use(express.session({ secret: 'my_precious' }));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
+// Use application-level middleware for common functionality, including
+// logging, parsing, and session handling.
+app.use(require('morgan')('combined'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
 
-
-
+// app.use(session({
+//   name: 'simpleLib.sess', store: sessStore, secret: sessionSecret, resave: true,
+//   saveUninitialized: false, cookie: {maxAge: 1000 * 60 * 15}}));
 
 // Configure the Facebook strategy for use by Passport.
 //
@@ -29,7 +36,8 @@ app.set('view engine', 'jade');
 passport.use(new Strategy({
     clientID: '500464776961653',
     clientSecret: '3259e9a43457a53b2713b854cf5587ac',
-    callbackURL: 'http://localhost:3000/auth/facebook/callback'
+    callbackURL: 'http://localhost:3000/auth/facebook/callback',
+    profileFields: ['id', 'displayName', 'emails', 'photos'],
   },
   function(accessToken, refreshToken, profile, cb) {
     // In this example, the user's Facebook profile is supplied as the user
@@ -38,7 +46,29 @@ passport.use(new Strategy({
     // allows for account linking and authentication with other identity
     // providers.
 
-    return cb(null, profile);
+    User.findOne({ id: profile.id }, function(err, user) {
+          if(err) {
+            console.log(err);  // handle errors!
+          }
+          if (!err && user !== null) {
+            return cb(null, user);
+          } else {
+            console.log("creating user ...");
+            user = new User({
+              id: profile.id,
+              name: profile.displayName
+            });
+            user.save(function(err) {
+              if(err) {
+                console.log(err);  // handle errors!
+              } else {
+                console.log("saving user ...");
+                return cb(null, user);
+              }
+            });
+          }
+        });
+    // return cb(null, profile);
   }));
 
 
@@ -86,6 +116,18 @@ app.get('/login',
     res.render('login');
   });
 
+app.get('/account', ensureAuthenticated, function(req, res){
+  User.findById(req.session.passport.user, function(err, user) {
+    console.log('in account');
+    if(err) {
+      console.log(err);  // handle errors
+    } else {
+      console.log('in account');
+      res.render('chat', { user: user});
+    }
+  });
+});
+
 app.get('/login/facebook',
   passport.authenticate('facebook'));
 
@@ -94,28 +136,28 @@ app.get('/auth/facebook',
   function(req, res) {
     console.log('without callback');
     //console.log(req.user );
-    res.redirect('/chat');
+    //res.redirect('/chat');
   });
 
 app.get('/auth/facebook/callback',
   passport.authenticate('facebook', { failureRedirect: '/login' }),
   function(req, res) {
+    res.redirect('/account');
+    // var newUser = {};
+    // newUser.id = req.user.id;
+    // newUser.name = req.user.displayName;
 
-    var newUser = {};
-    newUser.id = req.user.id;
-    newUser.name = req.user.displayName;
-
-    var myData = new User(newUser);
-    console.log(myData);
-    myData.save()
-        .then(item => {
-          console.log('##user saved to database');
-          console.log(item);
-            res.render('chat', { user: item, title: 'Chat' });
-        })
-        .catch(err => {
-            res.status(400).send("Unable to save to database");
-        });
+    // var myData = new User(newUser);
+    // console.log(myData);
+    // myData.save()
+    //     .then(item => {
+    //       console.log('##user saved to database');
+    //       console.log(item);
+    //         res.render('chat', { user: item, title: 'Chat' });
+    //     })
+    //     .catch(err => {
+    //         res.status(400).send("Unable to save to database");
+    //     });
 
   });
 
