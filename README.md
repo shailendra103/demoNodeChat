8be1ca6216e7d75c6fb71c38ba9a235a9c627cb6
Shailendra <shailendra@propertyguru.com.sg>
2017-07-11 02:45:42 +0800

    minor code cleanup

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# Saving Data to MongoDB Database from a Nodejs Application
-This is the finished code from a tutorial that I wrote about saving data to a MongoDB database from a Node.js Application. You can read the tutorial to get step-by-step instructions on how to save data to a MongoDB database.
+# simple Chat using Facebook login and storing data in mongodb
 
-[You can read the tutorial here](http://www.jenniferbland.com/saving-data-to-mongodb-database-from-node-js-application-tutorial/)
-# demoNodeChat
+After npm install access the app at http://localhost:3000/
--- app.js
+++ app.js
@@ -9,9 +9,7 @@ var io = require('socket.io').listen(server);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(express.static('public'));
-//app.use(express.session({ secret: 'my_precious' }));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
@@ -22,17 +20,8 @@ app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
 app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
 
-// app.use(session({
-//   name: 'simpleLib.sess', store: sessStore, secret: sessionSecret, resave: true,
-//   saveUninitialized: false, cookie: {maxAge: 1000 * 60 * 15}}));
 
 // Configure the Facebook strategy for use by Passport.
-//
-// OAuth 2.0-based strategies require a `verify` function which receives the
-// credential (`accessToken`) for accessing the Facebook API on the user's
-// behalf, along with the user's profile.  The function must invoke `cb`
-// with a user object, which will be set at `req.user` in route handlers after
-// authentication.
 passport.use(new Strategy({
     clientID: '500464776961653',
     clientSecret: '3259e9a43457a53b2713b854cf5587ac',
@@ -40,12 +29,6 @@ passport.use(new Strategy({
     profileFields: ['id', 'displayName', 'emails', 'photos'],
   },
   function(accessToken, refreshToken, profile, cb) {
-    // In this example, the user's Facebook profile is supplied as the user
-    // record.  In a production-quality application, the Facebook profile should
-    // be associated with a user record in the application's database, which
-    // allows for account linking and authentication with other identity
-    // providers.
-
     User.findOne({ id: profile.id }, function(err, user) {
           if(err) {
             console.log(err);  // handle errors!
@@ -73,14 +56,6 @@ passport.use(new Strategy({
 
 
 // Configure Passport authenticated session persistence.
-//
-// In order to restore authentication state across HTTP requests, Passport needs
-// to serialize users into and deserialize users out of the session.  In a
-// production-quality application, this would typically be as simple as
-// supplying the user ID when serializing, and querying the user record by ID
-// from the database when deserializing.  However, due to the fact that this
-// example does not have a database, the complete Facebook profile is serialized
-// and deserialized.
 passport.serializeUser(function(user, cb) {
   cb(null, user);
 });
@@ -92,7 +67,6 @@ passport.deserializeUser(function(obj, cb) {
 // session.
 app.use(passport.initialize());
 app.use(passport.session());
-
 
 
 //Mongoose setup
@@ -135,8 +109,6 @@ app.get('/auth/facebook',
   passport.authenticate('facebook', { failureRedirect: '/login' }),
   function(req, res) {
     console.log('without callback');
-    //console.log(req.user );
-    //res.redirect('/chat');
   });
 
 app.get('/auth/facebook/callback',
@@ -167,17 +139,7 @@ app.get('/profile',
     res.render('profile', { user: req.user });
   });
 
-//old routes
-// app.get("/", (req, res) => {
-//     res.sendFile(__dirname + "/index.html");
-// });
 
-
-// app.get('/chat', ensureAuthenticated, function(req, res){
-//   console.log('calling chat now' );
-//   console.log(req.user );
-//   res.render('chat', { user: req.user, title: 'Chat' });
-// });
 
 io.on('connection', function(client) {
   console.log('Client connected...');
