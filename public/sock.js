d48965ba2335fd6fef7df2f7ef7a1de93b733db3
Shailendra <shailendra@propertyguru.com.sg>
2017-07-11 15:06:10 +0800

    Chat screen cleaned up with username

--- public/sock.js
+++ public/sock.js
@@ -1,22 +1,23 @@
-
 var socket = io.connect('http://localhost:3000');
 
-  socket.on('connect', function(data) {
-      socket.emit('join', 'Hello server from client');
-  });
+socket.on('connect', function(data) {
+  socket.emit('join', 'Hello server from client');
+});
 
 
-  // listener for 'thread' event, which updates messages
-
+// listener for 'thread' event, which updates messages
 socket.on('thread', function(data) {
-    $('#thread').append('<li>' + data + '</li>');
-  });
+  $('#thread').append('<li>' + data.name + ' : '+ data.message + '</li>');
+});
 
-  // sends message to server, resets & prevents default form action
-  $('form').submit(function() {
-    var message = $('#message').val();
-    socket.emit('messages', message);
-    this.reset();
-    return false;
-  });
+// sends message to server, resets & prevents default form action
+$('form').submit(function() {
+  var data = {};
+  data.message = $('#message').val();
+  data.name = $('#name').val();
+
+  socket.emit('messages', data);
+  this.reset();
+  return false;
+});
 
