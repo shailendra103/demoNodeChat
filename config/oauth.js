b5b907d13ae630aed356666be4b0450053740bab
Shailendra <shailendra@propertyguru.com.sg>
2017-07-12 00:39:13 +0800

    minor formatting

--- config/oauth.js
+++ config/oauth.js
@@ -1,9 +1,9 @@
-var ids = {
-  facebook: {
-    clientID: '500464776961653',
-    clientSecret: '3259e9a43457a53b2713b854cf5587ac',
-    callbackURL: 'http://localhost:3000/auth/facebook/callback'
-  }
-};
+  var ids = {
+    facebook: {
+      clientID: '500464776961653',
+      clientSecret: '3259e9a43457a53b2713b854cf5587ac',
+      callbackURL: 'http://localhost:3000/auth/facebook/callback'
+    }
+  };
 
-module.exports = ids;
+  module.exports = ids;
